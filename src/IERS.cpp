cdad0d18bf5e3f0323bad9335d6d065532310e1c
frbartol <frbartol@unirioja.es>
2025-06-13 12:14:50 +0200

    Error en EFK

--- src/IERS.cpp
+++ src/IERS.cpp
@@ -8,6 +8,7 @@ tuple<double, double, double, double, double, double, double, double, double> IE
 
     if (interp =='l'){
         mjd = (floor(Mjd_UTC));
+        cout<<"mjd:"<<mjd<<endl;
         i = 0;
         for (int col = 1; col <= eop.n_column; col++) {
             if (mjd == eop(4, col)) {
